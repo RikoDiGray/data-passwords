d5fb7d50cae4009c4187185ffece7ddfcd2de9ab
ysff <57960431+RikoDiGray@users.noreply.github.com>
2020-12-27 02:13:46 +0300

    Update connect.js

--- data/connect.js
+++ data/connect.js
@@ -1,6 +1,6 @@
 function ffmdfmdjdsajsdjdjs() {
   $(function() {
-    $.getJSON('data/backup.json', function(connect) {
+    $.getJSON('data/base.json', function(connect) {
       $.each(connect.data, function(i, f) {
            create(f.date, f.type, f.title, f.login, f.password, f.site, f.content);
       });
